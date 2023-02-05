48d365667bf16e40bedf18e33a6cd258c9fce173
Phil Gathany <ngathany@gmail.com>
2023-02-04 22:48:28 -0500

    theme plays

---
+++ Assets/Scripts/Audio/AudioFade.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AudioFade
+{
+    public static IEnumerator FadeOut(Sound sound, float fadingTime, Func<float, float, float, float> Interpolate, Action Callback = null)
+    {
+        float startVolume = sound.Source.volume;
+        float frameCount = fadingTime / Time.deltaTime;
+        float framesPassed = 0;
+
+        while (framesPassed <= frameCount)
+        {
+            var t = framesPassed++ / frameCount;
+            sound.Source.volume = Interpolate(startVolume, 0, t);
+            yield return null;
+        }
+
+        sound.Source.volume = 0;
+        sound.Source.Stop();
+        sound.Source.volume = sound.Volume;
+        if (Callback != null)
+        {
+            Callback();
+        }
+    }
+    public static IEnumerator FadeIn(Sound sound, float fadingTime, Func<float, float, float, float> Interpolate, Action Callback = null)
+    {
+        sound.Source.Play();
+        sound.Source.volume = 0;
+
+        float resultVolume = sound.Volume;
+        float frameCount = fadingTime / Time.deltaTime;
+        float framesPassed = 0;
+
+        while (framesPassed <= frameCount)
+        {
+            var t = framesPassed++ / frameCount;
+            sound.Source.volume = Interpolate(0, resultVolume, t);
+            yield return null;
+        }
+
+        sound.Source.volume = resultVolume;
+        if (Callback != null)
+        {
+            Callback();
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] Sounds;
+    public static AudioManager Instance;
+    void Awake()
+    {
+        //DontDestroyOnLoad(gameObject);
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        //else
+        //{
+        //    Destroy(gameObject);
+        //}
+        foreach (var s in Sounds)
+        {
+            s.Source = gameObject.AddComponent<AudioSource>();
+            s.Source.clip = s.Clip;
+            s.Source.volume = s.Volume;
+            s.Source.pitch = s.Pitch;
+            s.Source.loop = s.Loop;
+            s.Source.playOnAwake = s.PlayOnAwake;
+            if (s.PlayOnAwake)
+            {
+                s.Source.Play();
+            }
+        }
+    }
+
+    public void Play(string Name)
+    {
+        var sound = GetSound(Name);
+        sound.Source.Play();
+    }
+
+    public void Stop(string Name)
+    {
+        var sound = GetSound(Name);
+        sound.Source.Stop();
+    }
+
+    public bool IsPlaying(string Name)
+    {
+        var sound = GetSound(Name);
+        return sound.Source.isPlaying;
+    }
+
+    public Sound GetSound(string Name)
+    {
+        var sound = Sounds.FirstOrDefault(s => s.Name == Name);
+        if (sound == null)
+        {
+            Debug.LogWarning("Sound \"" + Name + "\" not found");
+            return null;
+        }
+        return sound;
+    }
+
+    public void FadeIn(string Name, float FadeInTime = 3f, Action Callback = null)
+    {
+        var sound = GetSound(Name);
+        StartCoroutine(AudioFade.FadeIn(sound, FadeInTime, Mathf.SmoothStep, Callback));
+    }
+
+    public void FadeOut(string Name, float FadeOutTime = 3f, Action Callback = null)
+    {
+        var sound = GetSound(Name);
+        StartCoroutine(AudioFade.FadeOut(sound, FadeOutTime, Mathf.SmoothStep, Callback));
+    }
+}
+
+[System.Serializable]
+public class Sound
+{
+    public string Name;
+    public AudioClip Clip;
+    [Range(0f, 1f)]
+    public float Volume;
+    [Range(.1f, 3f)]
+    public float Pitch;
+    public bool Loop;
+    public bool PlayOnAwake = false;
+    [HideInInspector]
+    public AudioSource Source;
+}
